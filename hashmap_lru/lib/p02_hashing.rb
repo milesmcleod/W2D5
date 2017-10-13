554aa50d7c300777d279d9f5cefd63da515eefe0
Miles McLeod <milesmcleod123@gmail.com>
2017-10-13 13:28:27 -0700

    Phase 2 Hashing complete

--- hashmap_lru/lib/p02_hashing.rb
+++ hashmap_lru/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    str = ''
+    self.each_with_index do |el, idx|
+      hash_num = (idx + 1) * 42 * 99 / 3
+      str << (el * hash_num).to_s
+    end
+    str.to_i
   end
 end
 
 class String
   def hash
+    str = ''
+    self.chars.each_with_index do |letter, idx|
+      str << (letter.ord * idx * 17 / 3 * 43 + 6).to_s
+    end
+    str.to_i
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    str = ''
+    self.each do |k, v|
+      str << ((k.to_s.hash + v.to_s.hash) * 43874).to_s
+    end
+    str.to_i
   end
 end
